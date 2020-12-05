9d14df5267c827269ef5fb57c40c0b558cf5d4ea
hunt-roar <62547204+hunt-roar@users.noreply.github.com>
2020-12-06 02:50:26 +0300

    HW3
    HW3

---
+++ HW3/HW3.java
@@ -0,0 +1,117 @@
+package HW3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class HW3 {
+    public static char[][] map;
+    public static int sizeMapX;
+    public static int sizeMapY;
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+    public static final Random RANDOM = new Random();
+
+    public static final char HUMAN_DOT = 'X';
+    public static final char AI_DOT = 'O';
+    public static final char EMPTY_DOT = '_';
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            if (checkWin(HUMAN_DOT)) {
+                System.out.println("Human win!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Draw!");
+                break;
+            }
+            printMap();
+            aiTurn();
+            printMap();
+            if (checkWin(AI_DOT)) {
+                System.out.println("AI win!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Draw!");
+                break;
+            }
+        }
+        SCANNER.close();
+    }
+
+    public static void initMap() {
+        sizeMapX = 3;
+        sizeMapY = 3;
+        map = new char[sizeMapY][sizeMapX];
+        for (int y = 0; y < sizeMapY; y++) {
+            for (int x = 0; x < sizeMapX; x++) {
+                map[y][x] = EMPTY_DOT;
+            }
+        }
+    }
+
+    public static void printMap() {
+        for (int y = 0; y < sizeMapY; y++) {
+            for (int x = 0; x < sizeMapX; x++) {
+                System.out.print(map[y][x] + "|");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void humanTurn() {
+        int x;
+        int y;
+
+        do {
+            System.out.println("Введите координаты хода X, Y (от 1 до " + sizeMapX + ") через пробел: ");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!isValidCell(y, x) || !isEmptyCell(y, x));
+        map[y][x] = HUMAN_DOT;
+    }
+
+    public static void aiTurn() {
+        int x;
+        int y;
+
+        do {
+            x = RANDOM.nextInt(sizeMapX);
+            y = RANDOM.nextInt(sizeMapY);
+        } while (!isEmptyCell(y, x));
+        map[y][x] = AI_DOT;
+    }
+
+    public static boolean isValidCell(int y, int x) {
+        return x >= 0 && x < sizeMapX && y >= 0 && y < sizeMapY;
+    }
+
+    public static boolean isEmptyCell(int y, int x) {
+        return map[y][x] == EMPTY_DOT;
+    }
+
+    public static boolean checkWin(char symbol) {
+        for (int i =0; i<sizeMapY;i++){
+            if (map[i][0] == symbol && map[i][1] == symbol && map[i][2] == symbol) return true;
+            for (int j =0; j<sizeMapY;j++){
+                if (map[0][j] == symbol && map[1][j] == symbol && map[2][j] == symbol) return true;
+            }
+        }
+        if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
+        if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol) return true;
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int y = 0; y < sizeMapY; y++) {
+            for (int x = 0; x < sizeMapX; x++) {
+                if (map[y][x] == EMPTY_DOT) return false;
+            }
+        }
+        return true;
+    }
+}
